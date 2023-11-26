f842af02d0c752b0bc7c7749294d10011c2cfb57
ElijahUmana <elijah@uni.minerva.edu>
2023-11-26 15:58:16 -0500

    dd

--- frontend/src/components/About/DragDropFiles.js
+++ frontend/src/components/About/DragDropFiles.js
@@ -31,15 +31,12 @@ const DragDropFiles = ({
               inputRef.current.value = '';  // Reset the input element's value
           }
       }
-    }, [location.search, setFiles, setIsUploaded, handleFile, clearProcessedImage]);
+  }, [location.search]);  // Add location.search as a dependency
   
 
     useEffect(() => {
-        if (processedImage) {
-            setDisplayImage(processedImage);
-        }
+        setDisplayImage(processedImage || null);
     }, [processedImage]);
-    
 
     const handleDragOver = (event) => {
         event.preventDefault();
@@ -63,6 +60,7 @@ const DragDropFiles = ({
       await handleUpload(event.target.files);  // Automatically upload the file
   };
   
+  // ... rest of your code
   
   const handleUpload = async (files) => {
       if (!files || files.length === 0) {
@@ -112,36 +110,27 @@ const DragDropFiles = ({
 
     return (
         <>
-            {displayImage ? (
-                // If displayImage is set, show the image and the Remove Image button
-                <div className="dropzone">
-                    <img src={displayImage} className="uploaded-img" alt="Uploaded or processed" />
-                    <div className="actions">
-                        <button className="dropzone-button2" onClick={handleCancel}>Remove Image</button>
-                    </div>
-                </div>
-            ) : (
-                // If no image is set, show the upload interface
-                <div className="dropzone" onDragOver={handleDragOver} onDrop={handleDrop}>
-                    <h4>Utilize InsightAI's Chrome extension for easy screenshots, or upload image directly from your computer</h4>
-                    <br></br>
-                    <h5>We currently *ONLY* support images containing math equations, chemical equations, computer code snippets, or plain text.</h5>
-                    <input
-                        type="file"
-                        onChange={handleSelect}
-                        hidden
-                        accept="image/png, image/jpeg"
-                        ref={inputRef}
-                    />
-                    <button onClick={() => inputRef.current.click()} className="dropzone-button">
-                        Upload Image
-                    </button>
-                </div>
-            )}
+            <div className="dropzone" onDragOver={handleDragOver} onDrop={handleDrop}>
+                <h4>Utilize InsightAI's Chrome extension for easy screenshots, or upload image directly from your computer</h4>
+                <br></br>
+                <br></br>
+                <h5>We currently *ONLY* support images containing math equations, chemical equations, computer code snippets, or plain text.</h5>
+                <br></br>
+                <br></br>
+                <input
+                    type="file"
+                    onChange={handleSelect}
+                    hidden
+                    accept="image/png, image/jpeg"
+                    ref={inputRef}
+                />
+                <button onClick={() => inputRef.current.click()} className="dropzone-button">
+                    Upload Image
+                </button>
+            </div>
             <div className="actions"></div>
         </>
     );
-    
 };
 
 export default DragDropFiles;
